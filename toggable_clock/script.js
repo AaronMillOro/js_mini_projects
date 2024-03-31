9770e977b9a519c8be1a57262a338f4580df3833
Aaron MillOro <7230016+AaronMillOro@users.noreply.github.com>
2024-03-31 16:53:12 +0200

    :wrench: fixture on date fetching for the toggable clock app

--- toggable_clock/script.js
+++ toggable_clock/script.js
@@ -14,7 +14,7 @@ const months = [
 
 const days = {
     1: 'Monday', 2: 'Tuesday', 3: 'Wednesday', 4: 'Thursday', 
-    5: 'Friday', 6: 'Saturday', 7:'Sunday'
+    5: 'Friday', 6: 'Saturday', 0:'Sunday'
 }
 
 
